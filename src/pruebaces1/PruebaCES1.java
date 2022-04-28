cc5b61f53e09f610063694fb1051d6bb664f3db9
tovarhildeberto@gmail.com <>
2022-04-28 18:43:14 -0500

    Prueba CES 1

---
+++ src/pruebaces1/PruebaCES1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebaces1;
+
+/**
+ *
+ * @author Clases
+ */
+public class PruebaCES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GIT HUB TOVAR 2842022");
+    }
+    
+}
